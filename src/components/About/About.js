c111ba779345383a81d54560be5922cb1596a12a
Zakaria Bli <zakaria.bli99@gmail.com>
2023-03-21 17:06:33 +0100

    adding competion final section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import React from "react"
 import Container from "../UI/Container"
 
 import Map from "../../assets/wilaya.png"
+import finaleImg from "../../assets/ARC_logo.png"
 
 const About = () => {
   return (
@@ -26,7 +27,7 @@ const About = () => {
           qualité tout en garantissant un cadre de sécurité solide.
         </p>
       </section>
-      <section id="about" className="">
+      <section id="about">
         <h2 className="text-3xl text-Color-Cyan text-center font-bold uppercase py-10 mb-5">
           About the competition
         </h2>
@@ -38,8 +39,8 @@ const About = () => {
             <p className="text-white text-justify max-w-4xl mx-auto">
               The competition consists of two main phases: <br /> The first
               phase is the qualification for the final, and will take place on
-              29 April 2023 in the 4 following wilayas : Algiers, Ghardaia, Oran
-              and Batna. It involves a multitude of mini-challenges under
+              April, 29, 2023 in the 4 following wilayas : Algiers, Ghardaia,
+              Oran and Batna. It involves a multitude of mini-challenges under
               timers, involving skills in robotics but also basics in Artificial
               Intelligence that participants must have to make an effective
               selection of finalists.
@@ -48,6 +49,24 @@ const About = () => {
           <div>
             <img src={Map} alt="Competition_Map" className="w-[500px]" />
           </div>
+        </div>
+      </section>
+      <section className="flex justify-center items-center gap-10 ph:flex-col">
+        <div>
+          <img src={finaleImg} alt="Competition_Map" className="w-96" />
+        </div>
+        <div className="max-w-lg">
+          <h3 className="text-xl text-Color-Cyan font-bold uppercase py-5 ">
+            The final
+          </h3>
+          <p className="text-white text-justify max-w-4xl mx-auto">
+            The final will take place in the wilaya of Algiers, on July 22, 2023
+            where, the qualifiers of the first round will fight to win the 1st
+            title of "Algerian Robot Cup 2023" whose theme is the development of
+            an industrial plant and that, by integrating new technologies such
+            as : artificial intelligence, wireless communication protocols, new
+            mechanisms ... etc.
+          </p>
         </div>
       </section>
       <h2 className="text-3xl text-Color-Cyan text-center font-bold uppercase py-10 mb-5">
